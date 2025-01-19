b074b7fcd45d58bd23f961ab29b0c10526e21b93
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2025-01-19 13:19:44 +0300

    init: add webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import "./module.js";
+
+console.log("Hello world!");
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    index: "./index.js",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
